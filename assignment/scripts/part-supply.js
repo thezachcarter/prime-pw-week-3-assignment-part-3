1e9289151ed72612fe266a91fe5f19bcd4fd3906
Zach Carter <thezachcarter@gmail.com>
2022-02-02 14:55:09 -0600

    completed q 6

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -25,6 +25,7 @@ console.log(supplyChanges.pop());
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
+supplyChanges.push(25);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -32,6 +33,16 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.'
 console.log('6. Showing supplyChanges...');
+for ( let x=0; x<supplyChanges.length; x++ ){
+  if (supplyChanges[x]>0) {
+    console.log('Added ', supplyChanges[x], 'parts.');
+  } else if (supplyChanges[x]==0) {
+    console.log('No Change.');
+  } else {
+    console.log('Removed ', supplyChanges[x], 'parts.');
+  }
+// console.log(supplyChanges[x]);
+}
 
 
 // STRETCH GOALS
